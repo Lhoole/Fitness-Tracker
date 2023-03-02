ce53a7d579e52804482cf6799e19806a9d3a1405
Michael B <bosse730@gmail.com>
2023-03-02 19:32:30 +1100

    added route connectionss

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const exerciseRoutes = require('./exerciseRoutes');
+const mealRoutes = require('./mealRoutes');
+const sleepRoutes = require('./sleepRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/exercises', exerciseRoutes);
+router.use('/meals', mealRoutes);
+router.use('/sleeps', sleepRoutes);
+router.use('/users', userRoutes)
+
+module.exports = router;
