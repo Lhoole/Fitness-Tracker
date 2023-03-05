4c5440608292528bfe331d4a02477931e93feed6
andynewen <117054251+andynewen@users.noreply.github.com>
2023-03-05 17:04:42 +1100

    updated seed file

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,13 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Excercise } = require('../models');
-
-const userData = [
-  // Array of user data objects
-];
-
-const excerciseData = [
-  // Array of exercise data objects
-];
+const { User, Exercise, Fitness, Meal, Sleep } = require('../models');
+const userData = require('./user-seeds.json');
+const exerciseData = require('./exercise-seeds.json');
+const fitnessData = require('./fitness-seeds.json');
+const mealData = require('./meal-seeds.json');
+const sleepData = require('./sleep-seeds.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -17,15 +14,23 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const exercise of exerciseData) {
-    await Excercise.create({
-      ...exercise,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  const exercises = await Exercise.bulkCreate(exerciseData, {
+    returning: true,
+  });
+
+  const fitnesses = await Fitness.bulkCreate(fitnessData, {
+    returning: true,
+  });
+
+  const meals = await Meal.bulkCreate(mealData, {
+    returning: true,
+  });
+
+  const sleeps = await Sleep.bulkCreate(sleepData, {
+    returning: true,
+  });
 
   process.exit(0);
 };
 
 seedDatabase();
-
