dc50b715d547c40dba0321d97e20b763e746c3ec
adzzy89 <adam_pilato@hotmail.com>
2023-03-05 21:08:20 +1100

    changed file location in login.js

--- public/js/login.js
+++ public/js/login.js
@@ -37,7 +37,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/userData');
       } else {
         alert(response.statusText);
       }
